86e20a30eb8400fec715ba3fb5611ec1ddcd081a
lapin.aleksey.devs@gmail.com <NBAlive228@>
2025-04-30 02:00:32 +0300

    lara

---
+++ src/app/Models/Category.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+/**
+ * @property int $id
+ * @property string $category_name
+ */
+
+class Category extends Model
+{
+
+}
